9588d12f3a98267b2c20e38f34fa28114c2121fd
actionjack1309 <119631825+actionjack1309@users.noreply.github.com>
2022-12-10 22:28:02 -0500

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,9 @@
+from turtle import *
+speed(0.00000000000000000000000000000000000000000000000001)
+color('orange')
+bgcolor('black')
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
